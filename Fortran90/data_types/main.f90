75dc96422a17f9e37ef6ab2b267f734bcc4def6b
Emanuele Vignola <chronoceum@gmail.com>
2018-02-08 15:09:17 +0100

    First hybrid function added

--- Fortran90/data_types/main.f90
+++ Fortran90/data_types/main.f90
@@ -1,16 +1,17 @@
-        program la_forge        
+        program main 
         use commons
         use meso_approx
         implicit none
         integer, allocatable :: state(:)
-        type (approximation_type) :: obj_approx1
+        type (approximation_type) :: obj_approx
+        real*8 energy
 
+        ! Initialising the data structure of the model
         write(*,*) 'Approximation?'
-        read(*,*) obj_approx1%approx
-        call obj_approx1%init
-        
-        continue
-        
-        stop
-        
+        read(*,*) obj_approx%approx
+        call obj_approx%init
+        allocate(state(nsites)) 
+        state=1
+        write(*,*) energy(obj_approx,state)
+
         end program 
