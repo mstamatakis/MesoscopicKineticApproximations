8131173d39616618584a5a9fb6c1a71bcc2b01fc
Michail Stamatakis <m.stamatakis@ucl.ac.uk>
2018-02-09 16:19:48 +0000

    Use separate module for class instantiation
    This is done to avoid having the solver dealing with the instantiated object (even if it's just in the argument list of functions/subroutines in the solver).

---
+++ Fortran90/data_types/approx_inst.f90
@@ -0,0 +1,9 @@
+        module approx_inst
+        
+        use commons
+        use meso_approx
+        implicit none
+
+        type (approximation_type) :: obj_approx
+        
+        end module approx_inst
--- Fortran90/data_types/funcv.f90
+++ Fortran90/data_types/funcv.f90
@@ -1,30 +1,29 @@
-	subroutine funcv(appr,fvec)
+	subroutine funcv(fvec)
         ! This subroutine applies self-consistent constraints to the
         ! parameters. Its input are the number of parameters "n" and the
         ! vector of parameters "x", while "fvec" is the output. "fvec"
         ! is a function to be zeroed by the solver, which will find the
         ! self-consistent parameters this way.
         use commons
-        use meso_approx
+        use approx_inst
 	implicit none
         integer i, j, total
 	integer, allocatable, dimension(:) ::  v1, v2
 	real*8 fvec(npar), corr
-        type (approximation_type) :: appr
 	
         do i=1,npar
          total=0
          do j=1,nsites
-          if(appr%eqn%lhs(i,j).ne.0) then
+          if(obj_approx%eqn%lhs(i,j).ne.0) then
             total=total+1
           end if
          end do
          allocate(v1(total),v2(total)) 
          do j=1,total
-          v1(j)=appr%eqn%lhs(i,j)
-          v2(j)=appr%eqn%rhs(i,j)
+          v1(j)=obj_approx%eqn%lhs(i,j)
+          v2(j)=obj_approx%eqn%rhs(i,j)
          end do
-         fvec(i)=log(corr(v1,total,appr))-log(corr(v2,total,appr))
+         fvec(i)=log(corr(v1,total,obj_approx))-log(corr(v2,total,obj_approx))
          deallocate(v1,v2)
         end do
         do i=1,npar
