af2160fbdc33a99027ff79d3089af44526f737b7
mstamatakis <m.stamatakis@ucl.ac.uk>
2018-04-23 23:04:00 +0100

    Writing commands for checking Jacobian

--- FortranCode/fdjac.f90
+++ FortranCode/fdjac.f90
@@ -12,13 +12,13 @@ SUBROUTINE fdjac(x,fvec,df)
 			REAL(DP), DIMENSION(size(x)) :: funcv
 		END FUNCTION funcv
 	END INTERFACE
-	REAL(DP), PARAMETER :: EPS=1.0e-4_dp
+	REAL(DP), PARAMETER :: EPS=1.0e-6_dp
 	! Computes forward-difference approximation to Jacobian. On input, x is the point at which
 	! the Jacobian is to be evaluated, and fvec is the vector of function values at the point,
 	! both arrays of length N. df is the N × N output Jacobian. FUNCTION funcv(x) is a
 	! fixed-name, user-supplied routine that returns the vector of functions at x.
 	! Parameter: EPS is the approximate square root of the machine precision.
-	INTEGER(I4B) :: j,n
+	INTEGER(I4B) :: i,j,n
 	REAL(DP), DIMENSION(size(x)) :: xsav,xph,h
 	n=assert_eq(size(x),size(fvec),size(df,1),size(df,2),'fdjac')
 	xsav=x
@@ -30,6 +30,16 @@ SUBROUTINE fdjac(x,fvec,df)
 		x(j)=xph(j)
 		df(:,j)=(funcv(x)-fvec(:))/h(j) ! Forward difference formula.
 		x(j)=xsav(j)
-	end do
+    end do
+    
+    !write(*,*) ''
+    !do i=1,n
+    !    write(*,'(6ES30.16E3)') (df(i,j),j=1,n)
+    !enddo
+    !write(*,*) ''
+    !continue
+    
+    return
+    
 END SUBROUTINE fdjac	
 
